fc70993a5bf28115bd196ae8262e9a389f03dc9a
eshan <eshanidixit3@gmail.com>
2023-10-31 01:06:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/quizapplication/QuestionsAnswers.java
@@ -0,0 +1,18 @@
+package com.example.quizapplication;
+public class QuestionsAnswers {
+    public static String[] questions = {
+            "What is the capital of France?",
+            "Which planet is known as the Red Planet?",
+            "What is the currency of India?",
+            "When did India got its independence?",
+            "Which gas do plants absorb from the atmosphere during photosynthesis?"
+    };
+    public static String[][] options = {
+            {"Berlin", "Madrid", "Paris", "Rome"},
+            {"Venus", "Jupiter", "Mars", "Saturn"},
+            {"Dhiram", "Euro", "Rupee", "Dollar"},
+            {"1990", "1920", "1941", "1947"},
+            {"Carbon Dioxide(CO2)", "Oxygen(O2)", "Nitrogen(N2)", "Hydrogen(H2)"}
+    };
+    public static String[] answers = {"Paris", "Mars", "Rupee", "1947", "Carbon Dioxide(CO2)"};
+}
